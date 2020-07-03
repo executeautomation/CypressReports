0045cce6e59755da623652bc5bc6b7ab89c00949
karthik kk <karthik@techgeek.co.in>
2020-07-03 13:03:00 +1200

    Initial code checkIn

---
+++ cypress/support/index.js
@@ -0,0 +1,28 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+import addContext from 'mochawesome/addContext'
+
+Cypress.on("test:after:run", (test, runnable) => {
+
+    let videoName = Cypress.spec.name
+    videoName = videoName.replace('/.js.*', '.js')
+    const videoUrl = 'videos/' + videoName + '.mp4'
+
+    addContext({ test }, videoUrl)
+});
+
